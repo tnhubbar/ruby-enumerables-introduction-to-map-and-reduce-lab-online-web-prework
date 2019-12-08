e06f6d8b06485c070dbafa8ee4f5f184f99380ad
Taryn Johnson <taryn.johnson919@gmail.com>
2019-12-08 22:49:21 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,4 +1,22 @@
+# In this lab, we're going to write several `map`-like and `reduce`-like
+# methods and put them in `lib/my_code.rb`:
 
+# ### `map`-like
+
+# - `map_to_negativize(source_array)`
+# - `map_to_no_change(source_array)`
+# - `map_to_double(source_array)`
+# - `map_to_square(source_array)`
+
+# Remember, all `map` methods return a new `Array`.
+
+# ### `reduce`-like
+
+# - `reduce_to_total(source_array, starting_point)`
+# - `reduce_to_all_true(source_array)`
+# - `reduce_to_any_true(source_array)`
+
+# Remember, all `reduce` methods return a _value_.
 
 
 source_array = [2, 4, 6, 8]
