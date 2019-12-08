44f6435fb100800349c94a28a59611dce7a6d700
Taryn Johnson <taryn.johnson919@gmail.com>
2019-12-08 22:50:05 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -19,72 +19,70 @@
 # Remember, all `reduce` methods return a _value_.
 
 
-source_array = [2, 4, 6, 8]
 def map_to_negativize(source_array)
-  negativize = []
-i = 0
-while i < source_array.length do
-negativize<< (source_array[i] * -1) 
-i += 1 
-end 
-return negativize
+  new = []
+  i = 0
+  while i < source_array.length do
+    new.push( source_array[i] * -1 )
+    i += 1
+  end
+  return new
 end
 
 def map_to_no_change(source_array)
-  no_change = []
-  i = 0 
+  new = []
+  i = 0
   while i < source_array.length do
-    no_change.push (source_array[i] *1)
+    new.push( source_array[i] )
     i += 1
   end
-  return no_change
+  return new
 end
 
 def map_to_double(source_array)
-  double = []
-  i=0 
+  new = []
+  i = 0
   while i < source_array.length do
-    double.push (source_array[i] *2)
-    i += 1 
+    new.push( source_array[i] * 2 )
+    i += 1
   end
-  return double
+  return new
 end
 
 def map_to_square(source_array)
-  square = []
-  i = 0 
+  new = []
+  i = 0
   while i < source_array.length do
-    square.push (source_array[i]**2 )
-    i +=1 
-  end 
-  return square
-end 
+    new.push( source_array[i] * source_array[i] )
+    i += 1
+  end
+  return new
+end
 
-def reduce_to_total(source_array, starting_point=0 )
-  total = starting_point
-  i = 0 
-  while i < source_array.length do 
-    total = total + (source_array[i])
-    i +=1 
-  end 
-  return total
-end 
+def reduce_to_total(source_array, starting_point=0)
+  new = starting_point
+  i = 0
+  while i < source_array.length do
+    new += source_array[i]
+    i += 1
+  end
+  return new
+end
 
 def reduce_to_all_true(source_array)
-  i = 0 
-  while i < source_array.length do 
+  i = 0
+  while i < source_array.length do
     return false if !source_array[i]
-    i+=1 
-  end 
-return true 
+    i += 1
+  end
+  return true
 end
 
 def reduce_to_any_true(source_array)
-  i = 0 
+  i = 0
   while i < source_array.length do
     return true if source_array[i]
-    i+=1 
-  end 
-  
-  return false 
-end 
+    i += 1
+  end
+  return false
+end
